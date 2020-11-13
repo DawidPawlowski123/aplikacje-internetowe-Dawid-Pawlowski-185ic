462619d9adf07a248909141ba6853e1840faddb0
DawidP <dawidpawlowski98@wp.pl>
2020-11-13 13:55:13 +0100

    asdasdds

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING,)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
@@ -15,4 +16,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author}" 
