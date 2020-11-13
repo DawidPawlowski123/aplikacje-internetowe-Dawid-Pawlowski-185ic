8582f61b600b6c45dc84d603f755afdaad3c3c6f
DawidP <dawidpawlowski98@wp.pl>
2020-11-13 11:55:16 +0100

    asdasd

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
-from . import views
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    path('admin/', admin.site.urls),
 ]
