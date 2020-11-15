c13e0c65012f5921e886b11315ae5ef8430bafdd
DawidP <dawidpawlowski98@wp.pl>
2020-11-15 12:52:44 +0100

    Proba4

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posty': posts})
 
-def get_absolute_url(self):
-    return reverse("blog:post_detail",kwargs={'pk':self.pk})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
