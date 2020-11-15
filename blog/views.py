f08870361174acc2797fde3777fd4c2447f347da
DawidP <dawidpawlowski98@wp.pl>
2020-11-15 16:18:10 +0100

    Jazdaa

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,6 +31,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
