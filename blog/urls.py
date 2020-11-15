16b7596d8b5809a845308dfc623ccfcd959086d8
DawidP <dawidpawlowski98@wp.pl>
2020-11-15 12:32:34 +0100

    Proba4

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
-from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('/post/<int:pk>/', views.post_detail, name='post_detail'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('/admin/', admin.site.urls),
-    path('/blog/', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
