0d6ca4fdf7bb26cbbd09e8af5dc33e39d116a530
DawidP <dawidpawlowski98@wp.pl>
2020-11-15 11:22:44 +0100

    Proba3

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from .models import Post
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'blog/post_list.html', { 'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posty': posts})
