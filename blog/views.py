661d48400215ce0d114717b64084bb06cbb6164c
DawidP <dawidpawlowski98@wp.pl>
2020-11-15 16:08:46 +0100

    Jazda

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from .models import Post
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django import forms
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
