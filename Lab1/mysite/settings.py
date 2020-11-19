deacb11288822ebf2e475b094819863a3fd046a2
DawidP <dawidpawlowski98@wp.pl>
2020-11-19 23:45:52 +0100

    1

--- Lab1/mysite/settings.py
+++ Lab1/mysite/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog',     
+
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
